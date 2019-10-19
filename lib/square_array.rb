75a84e4349df188c5262af9d2ce20a09f0824d2d
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 02:13:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -13,7 +13,7 @@ def square_array (array)
     new_array =
     counter = 0
   while counter < array.length do
-    new_array <<  array[counter]
+    new_array <<  array[counter] **2
     counter += 1
   ## need a loop
   ##return new array
