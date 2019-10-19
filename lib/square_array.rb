44850b6d7e7c80bed421e436d0f1e26375fb9fa1
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 02:17:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,22 +1,10 @@
-#while as the iterator!!!
-#take an array of numbers
-#squre ecah number
-#return a ****new*** array with squared numbers
-
-#new_array= array**2
-#while <<<<condition>>>> do
-   ####execution code
-  #end
-
-
 def square_array (array)
     new_array =[]
     counter = 0
   while counter < array.length do
     new_array <<  array[counter] **2
     counter += 1
-  ## need a loop
-  ##return new array
+
   end
   new_array
 end
