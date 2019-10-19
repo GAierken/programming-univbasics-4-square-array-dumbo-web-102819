7cd50849d72fb48d5179cf51679b76871a1c36ea
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 01:58:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
-#while as the iterator
+#while as the iterator!!!
 #take an array of numbers
 #squre ecah number
-#return a new array with squared numbers
+#return a ****new*** array with squared numbers
 
 
 
